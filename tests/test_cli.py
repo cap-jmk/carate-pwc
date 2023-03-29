90dab069c4b8ebd9dff5d517ced341d57eb049b8
Julian Kleber <julian.kleber@sail.black>
2023-03-29 22:32:01 +0200

    refactor tests further

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,6 +5,7 @@ import subprocess
 
 from amarium.utils import search_subdirs
 
+from tests.utils import check_result_files
 
 def test_cli_func():
     if os.path.isdir("tests/data"):
@@ -19,47 +20,10 @@ def test_cli_func():
         stderr=subprocess.PIPE,
     )
     errs = result.stderr.decode()
-    print(errs)
-    # check result files
-    result_files, result_dirs = search_subdirs(dir_name="tests/results/ZINC_test/data")
-    reference_dirs = [
-        "tests/results/ZINC_test/data/CV_0",
-        "tests/results/ZINC_test/data/CV_1",
-    ]
-    assert len(result_dirs) == 2
-    for dir_name in result_dirs:
-        assert dir_name in reference_dirs
 
-    assert len(result_files) == 2
-    reference_files = [
-        "tests/results/ZINC_test/data/CV_0/ZINC_test.json",
-        "tests/results/ZINC_test/data/CV_1/ZINC_test.json",
-    ]
-    for name in result_files:
-        assert name in reference_files
-
-    # check result checkpoints
-    result_files, result_dirs = search_subdirs(
-        dir_name="tests/results/ZINC_test/checkpoints"
+    run_title = "ZINC_test"
+    data_set_name = "ZINC_test"
+    result_dir = f"tests/results/{run_title}"
+    check_result_files(
+        result_dir=result_dir, run_title=run_title, data_set_name=data_set_name
     )
-
-    reference_dirs = [
-        "tests/results/ZINC_test/checkpoints/CV_0",
-        "tests/results/ZINC_test/checkpoints/CV_1",
-    ]
-    assert len(result_dirs) == 2
-    for dir_name in result_dirs:
-        assert dir_name in reference_dirs
-
-    assert len(result_files) == 2
-    reference_files = [
-        "tests/results/ZINC_test/checkpoints/CV_0/ZINC_test.tar",
-        "tests/results/ZINC_test/checkpoints/CV_1/ZINC_test.tar",
-    ]
-    for name in result_files:
-        assert name in reference_files
-
-    result_dir_content = os.listdir("tests/results/ZINC_test/model_parameters")
-    result_dir_content_data = [x for x in result_dir_content if x.endswith(".json")]
-    assert len(result_dir_content_data) == 1
-    assert "model_architecture.json" in result_dir_content_data
