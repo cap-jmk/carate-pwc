9fa58bba769fcafabccf5490f4b03476d395d700
dev <dev@pendrive.pendrive>
2023-09-16 22:21:42 +0800

    add new models

--- carate/models/g_classification.py
+++ carate/models/g_classification.py
@@ -1,11 +1,3 @@
-"""
-cgc_classification model is named after the structure of the graph neural network.
-The graph neural network is structured with a convolutional , graph attention,
-and another convolutional layer. The cgc_classificatin model was the model
-tested int the publication Introducing CARATE: Finally speaking chemistry.
-
-:author: Julian M. Kleber
-"""
 import torch
 import torch.nn.functional as F
 from torch.nn import Linear
@@ -34,7 +26,6 @@ class Net(Model):
     The Net is the core algorithm and needs a constructor and a
     forward pass. The train, test and evaluation methods are implemented
     in the evaluation module with the Evaluation class.
-
     """
 
     def __init__(self, dim: int, num_features: int, num_classes: int) -> None:
@@ -42,9 +33,9 @@ class Net(Model):
             dim=dim, num_features=num_features, num_classes=num_classes
         )
 
+
         self.conv1 = GraphConv(self.num_features, self.dim)
         self.conv3 = GATConv(self.dim, self.dim, dropout=0.6, heads=16)
-        self.conv5 = GraphConv(self.dim * 16, self.dim)
 
         self.fc1 = Linear(self.dim, self.dim)
         self.fc2 = Linear(self.dim, self.num_classes)
@@ -53,9 +44,6 @@ class Net(Model):
         x = F.relu(self.conv1(x, edge_index, edge_weight))
         x = F.dropout(x, p=0.5, training=self.training)
         x = F.relu(self.conv3(x, edge_index, edge_weight))
-        x = F.relu(self.conv5(x, edge_index, edge_weight))
-        x = global_add_pool(x, batch)
-        x = F.relu(self.fc1(x))
         x = F.dropout(
             x, p=0.5, training=self.training
         ) 
