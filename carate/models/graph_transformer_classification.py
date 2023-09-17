c212dde896bc9fb21a6c3c803f6643a7ffb5e950
Julian M. Kleber <julian.kleber@sail.black>
2023-09-17 18:27:28 -0400

    add new models

--- carate/models/graph_transformer_classification.py
+++ carate/models/graph_transformer_classification.py
@@ -1,16 +1,8 @@
-"""
-cgc_classification model is named after the structure of the graph neural network.
-The graph neural network is structured with a convolutional , graph attention,
-and another convolutional layer. The cgc_classificatin model was the model
-tested int the publication Introducing CARATE: Finally speaking chemistry.
-
-:author: Julian M. Kleber
-"""
 import torch
 import torch.nn.functional as F
 from torch.nn import Linear
 
-from torch_geometric.nn import global_add_pool, GraphConv, GATConv
+from torch_geometric.nn import global_add_pool,  TransformerConv
 
 import numpy as np
 import sklearn.metrics as metrics
@@ -34,29 +26,27 @@ class Net(Model):
     The Net is the core algorithm and needs a constructor and a
     forward pass. The train, test and evaluation methods are implemented
     in the evaluation module with the Evaluation class.
-
     """
 
-    def __init__(self, dim: int, num_features: int, num_classes: int, heads:int=16) -> None:
+    def __init__(
+        self, dim: int, num_features: int, num_classes: int, heads: int = 16
+    ) -> None:
         super(Net, self).__init__(
             dim=dim, num_features=num_features, num_classes=num_classes
         )
         self.heads = heads
-        
-        self.conv1 = GraphConv(self.num_features, self.dim)
-        self.conv2 = GraphConv(self.dim * 16, self.dim)
+        self.conv3 = TransformerConv(self.num_features, self.dim,
+                             dropout=0.6, heads=self.heads)
 
-        self.fc1 = Linear(self.dim, self.dim)
+        self.fc1 = Linear(self.dim*self.heads, self.dim)
         self.fc2 = Linear(self.dim, self.num_classes)
 
     def forward(self, x, edge_index, batch, edge_weight=None):
-
-        x = F.relu(self.conv1(x, edge_index, edge_weight))
+        x = F.relu(self.conv3(x, edge_index, edge_weight))
         x = F.dropout(x, p=0.5, training=self.training)
-        x = F.relu(self.conv2(x, edge_index, edge_weight))
-        
         x = global_add_pool(x, batch)
+
         x = F.relu(self.fc1(x))
-        x = F.dropout(x, p=0.5, training=self.training)
         x = self.fc2(x)
-        return torch.sigmoid(x)
+        x = torch.sigmoid(x)
+        return x
