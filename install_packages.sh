100d987bbd3d4e3be9af684210223dfbec76dacf
Julian M. Kleber <julian.kleber@sail.black>
2023-09-21 17:42:43 -0400

    update the rtd yml

--- install_packages.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu 
-pip install torch-scatter torch-sparse -f https://data.pyg.org/whl/torch-1.13.0+${CUDA}.html
-pip install torch-geometric rdkit-pypi networkx[default] matplotlib
-pip install torch-cluster 
-pip install torch-spline-conv
